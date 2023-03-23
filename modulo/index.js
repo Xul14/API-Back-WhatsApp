500abc12a62c773c45c71c8872c8473cb67bd186
Xulia <juliasoaresdealmeidaj@gmail.com>
2023-03-23 15:17:45 -0300

    Concluído

--- modulo/index.js
+++ modulo/index.js
@@ -1,20 +1,20 @@
 /***********************************************************************
-* Objetivo: API Back-End WhatsApp
-* Autor: Julia Soares de Almeida.
-* Data: 19/03/23
-* Versão: 1.0
-***********************************************************************/
+ * Objetivo: API Back-End WhatsApp
+ * Autor: Julia Soares de Almeida.
+ * Data: 19/03/23
+ * Versão: 1.0
+ ***********************************************************************/
 
 var contatos = require('./contatos.js')
 
-const getIdPerfil = (function (id) {
+const getIdPerfil = (function(id) {
     let jsonContatos = {}
     let status = false
 
     contatos.contatos['whats-users'].forEach(perfil => {
 
-        if (contatos.id == id) {
-            jsonContatos.contatos = perfil.contacts
+        if (perfil.id == id) {
+            jsonContatos = perfil.contacts
             status = true
         }
     })
@@ -27,14 +27,14 @@ const getIdPerfil = (function (id) {
 
 })
 
-const getNumeroPerfil = (function (numeroTelefone) {
+const getNumeroPerfil = (function(numeroTelefone) {
     let jsonContatos = {}
     let status = false
 
     contatos.contatos['whats-users'].forEach(perfil => {
 
-        if (contatos.number == numeroTelefone) {
-            jsonContatos.contatos = perfil.contacts
+        if (perfil.number == numeroTelefone) {
+            jsonContatos = perfil.contacts
             status = true
         }
     })
